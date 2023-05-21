9931714536f8bfc624d5c0324ae7d5e07060fa55
Tomás Gil <tommygilbravo@gmail.com>
2023-05-20 21:38:07 -0400

    Lloré pero avances

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
 const btnClosePopup = document.querySelector("#close-popup");
 const popup = document.querySelector("#popup");
 let playerName = document.getElementById("player-name");
-var cartas = document.querySelectorAll('.card');
+let btnStart = document.getElementById("start-button");
 let maxScore = 1000;
+let sec = 45;
+let timer;
+let remainingTime = document.getElementById("remaining-time");
+
 
 
 const showPopup = () =>{
@@ -29,8 +33,51 @@ btnClosePopup.addEventListener("click", () =>{
 });
 
 
+const cartas = document.querySelectorAll('.card');
+
 [...cartas].forEach((carta)=>{
   carta.addEventListener( 'click', function() {
-    carta.classList.toggle('volteada');
+    carta.classList.toggle('flip');
   });
 });
+
+
+const genenerateBoard = ()=>{
+    [...cartas].forEach((carta)=>{
+        if (carta.classList.contains("flip")){
+            carta.classList.toggle('flip');
+        }
+      });
+
+    for(let i = 0; i<16; i++){
+        let card = document.getElementById(`tarjeta-${i+1}`);
+        
+        for(let j = 0; j < 8; j++){
+            card.classList.remove(`cara-reverso${j+1}`);
+        }
+    }
+
+    let numbers = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
+    numbers = numbers.sort(()=> {return Math.random() - 0.5});
+
+    for(let i = 0; i < 16; i++){
+        let reverse = document.getElementById(`tarjeta-${i+1}`);
+        reverse.classList.add(`cara-reverso${numbers[i]}`);
+    }
+};
+
+
+btnStart.addEventListener("click", ()=>{
+    genenerateBoard();
+    Timer()    
+})
+
+const Timer=()=>{
+    timer = setInterval(()=>{
+        sec--;
+        remainingTime.innerHTML = `Tiempo Restante: ${sec} s.`;
+        if(sec <= 0){
+            clearInterval(timer);
+        }
+    }, 1000);
+}
