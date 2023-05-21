0198e36108058db72d507887eef740ae8ce65d99
Tomás Gil <tommygilbravo@gmail.com>
2023-05-21 16:19:28 -0400

    Juego Finalizado

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
 const btnClosePopup = document.querySelector("#close-popup");
+const btnCloseScoreboard = document.querySelector("#close-popup2");
 const popup = document.querySelector("#popup");
+const popup2 = document.querySelector("#popup2")
 let playerName = document.getElementById("player-name");
 let btnStart = document.getElementById("start-button");
 let btnReset = document.getElementById("reset-button");
 let btnScore = document.getElementById("score-button");
+let scoreBoard = document.getElementById("scoreBoard")
 let blockedBoard = true;
 let maxScore = 1000;
 let sec = 181;
@@ -11,13 +14,54 @@ let found = 0;
 let timer;
 let remainingTime = document.getElementById("remaining-time");
 let scoreIndicator = document.getElementById("score");
+let usuarios = [];
+let antiguos;
 
+
+const save = (finalscore) =>{
+    let user = {name: `${playerName.innerHTML}`, score:finalscore};
+    usuarios.push(user);
+    localStorage.setItem("users",JSON.stringify(usuarios));
+}
+
+const cargar = () =>{
+
+    antiguos=JSON.parse(localStorage.getItem("users"));
+    if(antiguos !=null){
+        usuarios = JSON.parse(localStorage.getItem("users"));
+    }
+
+    usuarios.sort(function(a,b){
+        return b.score - a.score;
+    })
+
+    let text=[]
+    console.log(usuarios.length);
+
+    for(let i = 0; i< usuarios.length; i++){
+        text.push(`
+        <li class="listElement">${usuarios[i].name}:${usuarios[i].score}</li>
+        `);
+    }
+
+    scoreBoard.innerHTML = text.join(" ")
+    console.log(usuarios);
+
+    
+}
 
 const showPopup = () =>{
     popup.showModal();
 };
 
+const showScoreboard = () =>{
+    popup2.showModal();
+}
+
+
 document.onload = showPopup();
+document.onload = popup2.close();
+document.onload = cargar();
 
 window.onkeydown = function(e){
     if(e.keyCode === 27){
@@ -33,6 +77,16 @@ btnClosePopup.addEventListener("click", () =>{
     }else{        
         popup.close();
         playerName.innerHTML= `Jugador: ${text}`;
+    }
+});
+
+btnCloseScoreboard.addEventListener("click", () =>{
+    popup2.close();
+});
+
+btnScore.addEventListener("click", ()=>{
+    if(blockedBoard == true){        
+        showScoreboard();
     }
 });
 
@@ -111,6 +165,7 @@ const Timer=()=>{
             let finalscore = Math.round(maxScore * (sec/181));
             alert(`Juego Terminado! Puntuación Obtenida: ${finalscore}`)
             scoreIndicator.innerHTML = `Puntuación: ${finalscore}`
+            save(finalscore);
         }
     }, 1000);
 }
@@ -144,21 +199,23 @@ const deselect = (selections) =>{
         let tarjeta1 = document.getElementById(`card-${selections[0]}`);
         let tarjeta2 = document.getElementById(`card-${selections[1]}`);
 
-        let str1 = card1.classList.toString().replace(" flip", "");
-        let str2 = card2.classList.toString().replace(" flip", "");
-        console.log(str1);
-        console.log(str2);
 
-        if(str1 == str2){
+        if(tarjeta1 != tarjeta2){
+            let str1 = card1.classList.toString().replace(" flip", "");
+            let str2 = card2.classList.toString().replace(" flip", "");
 
-            tarjeta1.classList.add("absolute-flip");
-            tarjeta2.classList.add("absolute-flip");
-            found += 1;
-            
-        }else{
-            tarjeta1.classList.toggle("flip");
-            tarjeta2.classList.toggle("flip");
-        }        
+            if(str1 == str2){
+
+                tarjeta1.classList.add("absolute-flip");
+                tarjeta2.classList.add("absolute-flip");
+                found += 1;
+                
+            }else{
+                tarjeta1.classList.toggle("flip");
+                tarjeta2.classList.toggle("flip");
+            }
+        }  
+                
     }, 1000);
     setTimeout(()=>{
 
